375ada3dd9b290fd65034556728e69c69df3aca1
italosantana <flaubert165@gmail.com>
2019-12-29 13:53:18 -0300

    init

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.IO;
+using Confluent.Kafka;
+
+namespace TesteKafka
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            DateTime begin = DateTime.Now;
+
+            string path = @"/Users/italosantana/Desktop/TesteKafka/Output.txt";
+            
+            var conf = new ProducerConfig { BootstrapServers = "localhost:31090", QueueBufferingMaxMessages = 1000000 };
+
+            Action<DeliveryReport<Null, string>> handler = r =>
+            {
+                using (StreamWriter sw = File.AppendText(path))
+                {
+                    sw.WriteLine($"Delivered message in {(DateTime.Now - begin).TotalSeconds.ToString()} seconds");
+                }
+            };
+
+            
+            using (var p = new ProducerBuilder<Null, string>(conf).Build())
+            {
+                Console.WriteLine(begin.ToString("dd/MM/yyyy hh:mm:ss.fff tt"));
+
+                for (int i=0; i<10; ++i)
+                {
+                    p.Produce("bar", new Message<Null, string> { Value = i.ToString() }, handler);
+                }
+
+                p.Flush(TimeSpan.FromSeconds(10)); 
+            }
+        }
+    }
+}
